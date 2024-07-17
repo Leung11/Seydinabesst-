e518d4493b8ba43120f2c9d24a8985dceeedcea8
Leung11 <148726613+Leung11@users.noreply.github.com>
2024-07-17 16:46:08 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
+global.ownername = "𝓢𝓮𝔂𝓭𝓲𝓷𝓪🤴🏽🌪️";
+global.owner = ['7740961950'];
 global.premium = ['917050906659'];
-global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.packname = "𝓢𝓮𝔂𝓭𝓲𝓷𝓪🤴🏽🌪️";
+global.author = "𝓢𝓮𝔂𝓭𝓲𝓷𝓪🤴🏽🌪️";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
 global.menuType = "2"
-global.autoswview = true
+global.autoswview = false
 global.autoread = false
 global.available = false
 global.autoTyping = false
@@ -24,8 +24,8 @@ global.autoRecord = false
 global.autoreact = false
 global.onlygroup = false
 global.onlypc = false
-global.welcome = true
-global.autoBlock = true
+global.welcome = false
+global.autoBlock = false
  
 /*REPLY MESSAGE*/
 global.mess = {
@@ -34,7 +34,7 @@ global.mess = {
     success: '`✓ Success`',
     admin: '`This feature is only for group admins`',
     botAdmin: '`I am not an admin!`',
-    owner: '`You are not my owner`',
+    owner: '`Tu est pas Seydina`',
     group: '`You can use this command only in groups ❌`',
     private: '`Feature is used only for private chats!`',
     bot: '`Bot number user special features`',
